6104a3b911f3d563badb228894d9892beb38eb23
jam <jam.chenjun@gmail.com>
2015-10-26 16:29:26 +0800

    1. add readme.md; 2. add another view in the main view

--- ConstraintsInScrollView/CJMainViewController.m
+++ ConstraintsInScrollView/CJMainViewController.m
@@ -10,6 +10,12 @@
 
 @interface CJMainViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIView *firstView;
+@property (weak, nonatomic) IBOutlet UIView *secondView;
+
+@property (strong, nonatomic) UIView *thirdView;
+
 @end
 
 @implementation CJMainViewController
@@ -17,7 +23,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // add a thirdView
+    self.thirdView = [[UIView alloc] init];
+    self.thirdView.backgroundColor = [UIColor purpleColor];
+    [self.containerView addSubview:self.thirdView];
     
+    self.thirdView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.thirdView
+                                  attribute:(NSLayoutAttributeLeading)
+                                  relatedBy:(NSLayoutRelationEqual)
+                                     toItem:self.containerView
+                                  attribute:(NSLayoutAttributeLeading)
+                                 multiplier:1
+                                   constant:40]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.thirdView
+                                  attribute:(NSLayoutAttributeTop)
+                                  relatedBy:(NSLayoutRelationEqual)
+                                     toItem:self.secondView
+                                  attribute:(NSLayoutAttributeBottom)
+                                 multiplier:1
+                                   constant:100]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.thirdView
+                                  attribute:(NSLayoutAttributeTrailing)
+                                  relatedBy:(NSLayoutRelationEqual)
+                                     toItem:self.containerView
+                                  attribute:(NSLayoutAttributeTrailing)
+                                 multiplier:1
+                                   constant:-40]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.thirdView
+                                  attribute:(NSLayoutAttributeHeight)
+                                  relatedBy:(NSLayoutRelationEqual)
+                                     toItem:self.containerView
+                                  attribute:(NSLayoutAttributeHeight)
+                                 multiplier:0
+                                   constant:128]];
+    [self.view addConstraint:
+     [NSLayoutConstraint constraintWithItem:self.containerView
+                                  attribute:(NSLayoutAttributeBottom)
+                                  relatedBy:(NSLayoutRelationEqual)
+                                     toItem:self.thirdView
+                                  attribute:(NSLayoutAttributeBottom)
+                                 multiplier:1
+                                   constant:65]];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ readme.md
@@ -0,0 +1,3 @@
+Notes:
+
+The bottom constraint of the secondView is a placeholder constraint, and will be removed at build time.
